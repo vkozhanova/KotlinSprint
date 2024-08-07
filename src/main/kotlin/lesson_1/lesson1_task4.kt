05a822840b9c20c14f1f37314686ba3f77910920
vkozhanova <kozhanova_vera@mail.ru>
2024-08-07 14:22:58 +0500

    KS-1-4

---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_1
+fun main() {
+    val lenght:String = "40_868_600_00"
+    val age:Byte = 27
+    val partOfDay:Double = 0.075
+    val seconds:Short = 6480
+    val partOfYear:String = "2.0547945205479453E-4"
+    var apogee:Int = 327000
+
+    println("$lenght\n$age\n$partOfDay\n$seconds\n$partOfYear\n$apogee")
+}
