6ca999ae90b829612977ad2fab5cbcd7ec8d2ce5
vkozhanova <kozhanova_vera@mail.ru>
2024-08-08 21:29:41 +0500

    KS-2-1

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_2
+fun main() {
+    val student1 = 3.0
+    val student2 = 4.0
+    val student3 = 3.0
+    val student4 = 5.0
+
+    val medianScore = (student1 + student2 + student3 + student4) / 4
+    println("Средний балл: $medianScore")
+}
