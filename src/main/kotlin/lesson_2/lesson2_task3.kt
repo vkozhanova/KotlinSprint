57c991c1b6c645dca54973d16d76b9a06faae823
vkozhanova <kozhanova_vera@mail.ru>
2024-08-08 21:44:44 +0500

    KS-2-2-payment

---
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_2
+fun main() {
+    val departureTime = (9 * TIME) + 39
+    val travelTime = 457
+    val arrivalTime = departureTime+travelTime
+    val arrivalHour = arrivalTime / TIME
+    val arrivalMinutes = arrivalTime - (arrivalHour * TIME)
+
+    println("Время прибытия поезда: $arrivalHour:$arrivalMinutes")
+}
+const val TIME = 60
