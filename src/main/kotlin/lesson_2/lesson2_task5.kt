d16b5899892b92d3c9b83846a66ce882145195fe
vkozhanova <kozhanova_vera@mail.ru>
2024-08-09 15:01:38 +0500

    KS-2-5-ref

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -2,14 +2,11 @@ package org.example.lesson_2
 
 import kotlin.math.floor
 import kotlin.math.pow
-
-
 fun main() {
     val clientSum = 70000
     val rate = 16.7
     val timeInterval = 20
 
-    val futureClientSum = floor(clientSum*(1 + (rate / 100)).pow(timeInterval.toDouble()) * 1000.0) / 1000.0
-
-    println("Размер вклада составит $futureClientSum")
+    val futureClientSum = clientSum * (1 + (rate / 100)).pow(timeInterval)
+    println("Размер вклада составит %.3f".format(futureClientSum))
 }
