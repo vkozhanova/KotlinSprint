258c9ac960b9939dbe8b6d592d1dd901781f05e3
vkozhanova <kozhanova_vera@mail.ru>
2024-08-12 16:19:06 +0500

    KS-3-4

---
+++ src/main/kotlin/lesson3_task4.kt
@@ -0,0 +1,15 @@
+package org.example
+
+fun main() {
+    val from = "E2"
+    val to = "E4"
+    val moveNum = 0
+
+    val moveString = println("$from-$to;$moveNum")
+
+    val newFrom = "D2"
+    val newTo = "D3"
+    val newMoveNum = 1
+
+    val newMoveString = println("$newFrom-$newTo;$newMoveNum")
+}
