6f41db335eef0a7f7b0af58cb53d725686e71052
vkozhanova <kozhanova_vera@mail.ru>
2024-08-14 20:25:43 +0500

    KS-5-5

---
+++ src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -0,0 +1,26 @@
+package org.example.lesson_5
+import java.util.Random
+
+fun main() {
+    val random = Random()
+    val secretNums = mutableListOf(random.nextInt(43), random.nextInt(43), random.nextInt(43))
+    var inputNums = mutableListOf<Int>()
+
+    println("Введите три числа от 0 до 42:")
+
+    for (i in 1..3) {
+        val nums = readLine()!!.toInt()
+        inputNums.add(nums)
+    }
+
+    val matches = secretNums.intersect(inputNums).size
+
+    when (matches) {
+        3 -> println("Вы угадали все числа и выиграли джек-пот!")
+        2 -> println("Вы угадали два числа и получите крупный приз!")
+        1 -> println("Вы угадали одно число и получите утешительный приз!")
+        else -> println("Вы не угадали ни одного числа.")
+    }
+
+    println("Выигрышные числа были: ${secretNums}")
+}
