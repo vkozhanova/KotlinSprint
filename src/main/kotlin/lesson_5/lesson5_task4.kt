01db2592135fecbe844def1198361a02d8784551
vkozhanova <kozhanova_vera@mail.ru>
2024-08-14 18:45:12 +0500

    KS-5-4

---
+++ src/main/kotlin/lesson_5/lesson5_task4.kt
@@ -0,0 +1,30 @@
+package org.example.lesson_5
+
+fun main() {
+    val userName = "Zaphod"
+    val userPassword = "PanGalactic"
+
+    println("Пожалуйста, введите ваше имя:")
+    val inputName = readLine()!!
+
+    if (inputName == "Zaphod") {
+        println("Введите пароль:")
+        val inputPassword = readLine()!!
+        if (inputPassword == userPassword) {
+            println("Добро пожаловать, $userName! Вы имеете доступ к управлению кораблем.")
+
+        } else {
+            println("Неверный пароль.")
+        }
+    } else {
+        println("Пользователь не найден. Хотите зарегистрироваться? (да / нет)")
+
+
+        val inputResp = readLine()!!
+        if (inputResp == "да") {
+            println("Регистрация завершена.")
+        } else {
+            println("Хорошо, до свидания!")
+        }
+    }
+}
