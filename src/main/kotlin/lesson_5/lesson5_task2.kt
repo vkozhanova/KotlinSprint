5f4544f86561f85931140ab06864beb31c1e021a
vkozhanova <kozhanova_vera@mail.ru>
2024-08-14 17:26:05 +0500

    KS-5-2

---
+++ src/main/kotlin/lesson_5/lesson5_task2.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_5
+import java.time.LocalDate
+
+fun main() {
+    println("Введите свой год рождения:")
+
+     val userYear = readLine()?.toIntOrNull() ?: 0
+    val currentYear = LocalDate.now().year
+    var resultAge = currentYear - userYear
+
+    if ((resultAge <= AGE_OF_MAGORITY) || (userYear == 0)) {
+        println("Главный главный экран")
+    } else {
+        println("Показать скрытый контент")
+    }
+}
+
+const val AGE_OF_MAGORITY = 18
