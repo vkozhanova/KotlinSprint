15c9257b47a2dc3d0aaacd34d941a83f29fada8b
vkozhanova <kozhanova_vera@mail.ru>
2024-08-15 16:38:29 +0500

    KS-5-1-ref

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,11 +1,13 @@
 package org.example.lesson_5
 
 fun main() {
-    println("Для доступа к приложению подтвердите, что вы человек. Введите ответ 3 + 3 =")
+    val num1 = 3
+    val num2 = 3
+    println("Для доступа к приложению подтвердите, что вы человек. Введите ответ $num1 + $num2 =")
 
-    val result = readLine()?.toIntOrNull() ?: 0
+    val result = readlnOrNull()?.toIntOrNull()
 
-    if (result == 6) {
+    if (result == (num1 + num2)) {
         println("Добро пожаловать!")
     } else {
         println("Доступ запрещен.")
