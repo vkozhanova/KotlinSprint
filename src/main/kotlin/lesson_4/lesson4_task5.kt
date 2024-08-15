8b0887cad04d404be6ed54ab9cf4a8f6758c89db
vkozhanova <kozhanova_vera@mail.ru>
2024-08-15 13:42:18 +0500

    KS-4-5-ref

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -13,12 +13,16 @@ fun main() {
     println("Сообщите о благоприятности метеоусловий (true/false):")
     val isWeatherAlright = readLine()?.toBoolean() ?: false
 
-    val canGo = (!hasDamage && crew in 55..70 && provision > 50 && isWeatherAlright) ||
-            (hasDamage && crew == 70 && isWeatherAlright && provision >= 50)
+    val canGo = (!hasDamage && crew in REC_CREW && provision > REC_PROVISION && isWeatherAlright) ||
+            (!hasDamage && crew in REC_CREW && provision > REC_PROVISION) ||
+            (hasDamage && crew == REC_CREW.last && isWeatherAlright && provision >= REC_PROVISION)
 
     if (canGo) {
         println("Корабль может отправиться в плавание.")
     } else {
-        println("Корабль не может отправться в плавание.")
+        println("Корабль не может отправиться в плавание.")
     }
 }
+
+val REC_CREW = 55..70
+const val REC_PROVISION = 50
