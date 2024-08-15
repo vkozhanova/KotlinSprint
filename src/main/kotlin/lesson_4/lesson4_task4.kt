56cf52b83bce67f65753dcefb97563d72c334ea3
vkozhanova <kozhanova_vera@mail.ru>
2024-08-15 14:00:33 +0500

    KS-4-4-ref

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -2,14 +2,13 @@ package org.example.lesson_4
 
 fun main() {
     val day = 5
-    val isArmsAbsDay = day % 2 != 0
-    val isLegsBackDay = day % 2 == 0
+    val isEven = day % 2 == 0
 
     val result = """
-        Упражнения для рук: $isArmsAbsDay
-        Упражнения для ног: $isLegsBackDay
-        Упражнения для спины: $isLegsBackDay
-        Упражнения для пресса: $isArmsAbsDay
+        Упражнения для рук: ${!isEven}
+        Упражнения для ног: $isEven
+        Упражнения для спины: $isEven
+        Упражнения для пресса: ${!isEven}
     """.trimIndent()
 
     println(result)
