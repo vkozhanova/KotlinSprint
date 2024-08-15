216d50a553f8a5eb55cf60c64438cf7bab49aadc
vkozhanova <kozhanova_vera@mail.ru>
2024-08-15 17:18:05 +0500

    KS-5-3-ref

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -5,11 +5,11 @@ fun main() {
     val num2 = 17
 
     println("Введите первое число от 0 до 42:")
-    val userNum1 = readLine()?.toIntOrNull() ?: 0
+    val userNum1 = readln().toIntOrNull()
     println("Введите второе число от 0 до 42:")
-    val userNum2 = readLine()?.toIntOrNull() ?: 0
+    val userNum2 = readln().toIntOrNull()
 
-    if ((userNum1 == num1 && userNum1 == num2) || (userNum1 == num2 && userNum2 == num1)) {
+    if ((userNum1 == num1 && userNum2 == num2) || (userNum1 == num2 && userNum2 == num1)) {
         println("Вы выиграли главный приз!")
     } else if ((userNum1 == num1) || (userNum2 == num1) || (userNum1 == num2) || (userNum2 == num1)) {
         println("Вы выиграли утешительный приз!")
