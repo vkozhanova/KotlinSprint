71e67d64d0c969db90b370af376bb76a1adab009
vkozhanova <kozhanova_vera@mail.ru>
2024-08-15 13:15:39 +0500

    KS-4-3-ref

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -6,6 +6,12 @@ fun main() {
     val humidity = 20
     val currentSeason = "зима"
 
-    val condition = isSunny && isTentOpen && (humidity == 20) && (currentSeason != "зима")
+    val condition = (isSunny == COR_SUN_CONDITION) && (isTentOpen == COR_TENT_CONDITION) &&
+            (humidity == COR_HUMIDITY) && (currentSeason != WRONG_SEASON)
     println("Благоприятные ли сейчас условия для роста бобовых? $condition")
 }
+
+const val COR_HUMIDITY = 20
+const val COR_TENT_CONDITION = true
+const val COR_SUN_CONDITION = true
+const val WRONG_SEASON = "зима"
