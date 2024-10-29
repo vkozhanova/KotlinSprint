f9e7eb154d01da0fffaecff9df729b58137f1fcb
vkozhanova <kozhanova_vera@mail.ru>
2024-10-29 15:45:41 +0500

    KS-6-2

---
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_6
+
+fun main() {
+    println("Введите отсчетное время в секундах.")
+    val inputTime = readLine()?.toInt()?: 0
+    var userTime = inputTime
+
+    do {
+        Thread.sleep(1000)
+        userTime--
+
+    } while (userTime > 0)
+    println("Прошло $inputTime сек.")
+
+}
