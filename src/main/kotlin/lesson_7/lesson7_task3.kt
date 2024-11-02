be599f02cc750d41b76adbb0e08f51aecf27647d
vkozhanova <kozhanova_vera@mail.ru>
2024-11-02 19:02:34 +0500

    KS-7-3

--- src/main/kotlin/lesson_7/lesson7_task3.kt
+++ src/main/kotlin/lesson_7/lesson7_task3.kt
@@ -4,9 +4,7 @@ fun main() {
     println("Введите число:")
     val number = readln().toInt()
 
-    val range = 0..number
-
-    for (i in range step 2) {
+    for (i in 0..number step 2) {
         println(i)
     }
 }
