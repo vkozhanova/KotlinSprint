2d1a21d28b5c9a626cb86592d423d5d0855ffd46
vkozhanova <kozhanova_vera@mail.ru>
2024-11-06 18:29:12 +0500

    KS-11-4

---
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -0,0 +1,30 @@
+package org.example.lesson_11
+
+class CategoryDish (
+    val Id: Int,
+    val name: String,
+    val icon: String,
+    val description: String,
+)
+
+class Recipe (
+    val Id: Int,
+    val name: String,
+    val icon: String,
+    val isFavorite: Boolean = false,
+    val complexity: String,
+    val vegan: Boolean,
+    val gluten: Boolean,
+    val cookingMethod: String,
+    val time: String,
+    val mainIngredient: String,
+    val region: String,
+    val calorific: String,
+)
+
+class Ingredient (
+    val Id: Int,
+    val name: String,
+    val quantity: String,
+    val measurementUnit: String,
+)
