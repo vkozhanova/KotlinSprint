e64cee733595a4abb7d9a9d55cf2ae54ffdd7725
vkozhanova <kozhanova_vera@mail.ru>
2024-11-07 17:44:20 +0500

    KS-9-3

--- src/main/kotlin/lesson_9/lesson9_task3.kt
+++ src/main/kotlin/lesson_9/lesson9_task3.kt
@@ -2,11 +2,16 @@ package org.example.lesson_9
 
 fun main() {
     val omeletteAmount: List<Int> = listOf(2, 50, 15)
+
     println("Введите количество порций для рассчета:")
     val userInput = readln().toInt()
     val userAmount = omeletteAmount.map { it * userInput }
+
     println("На количество порций $userInput вам понадобится:" +
-            " яиц - ${userAmount[0]}шт," +
-            " молока - ${userAmount[1]}мл," +
-            " сливочного масла - ${userAmount[2]}гр.")
+            " яиц - ${userAmount[EGG_INDEX]}шт," +
+            " молока - ${userAmount[MILK_INDEX]}мл," +
+            " сливочного масла - ${userAmount[BUTTER_INDEX]}гр.")
 }
+const val EGG_INDEX = 0
+const val MILK_INDEX = 1
+const val BUTTER_INDEX = 2
