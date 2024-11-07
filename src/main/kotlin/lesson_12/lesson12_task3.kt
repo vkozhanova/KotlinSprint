69077480f7baf816e3f85f7f83245127c7957776
vkozhanova <kozhanova_vera@mail.ru>
2024-11-07 22:04:39 +0500

    KS-12-3

---
+++ src/main/kotlin/lesson_12/lesson12_task3.kt
@@ -0,0 +1,23 @@
+package org.example.lesson_12
+
+class WeatherRep(kelvinsDay: Int, kelvinsNight: Int) {
+
+    var dayTemperature: Int = kelvinsDay - KELVIN_FACTOR
+    var nightTemperature: Int = kelvinsNight - KELVIN_FACTOR
+    var isPrecipitation: Boolean = false
+
+    fun printWeatherReport() {
+        println("Дневная температура: $dayTemperature°C, Ночная температура: $nightTemperature°C, Наличие осадков: $isPrecipitation")
+    }
+}
+const val KELVIN_FACTOR = 273
+
+fun main() {
+
+    val day1 = WeatherRep(273, 263).apply { isPrecipitation = true }
+    val day2 = WeatherRep(276, 260).apply { isPrecipitation = false }
+
+    day1.printWeatherReport()
+    day2.printWeatherReport()
+}
+
