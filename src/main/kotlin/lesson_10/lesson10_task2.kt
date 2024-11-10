08342dcef8964223b3f9d2df724da9873ea34cb1
vkozhanova <kozhanova_vera@mail.ru>
2024-11-10 14:26:14 +0500

    KS-10-2

--- src/main/kotlin/lesson_10/lesson10_task2.kt
+++ src/main/kotlin/lesson_10/lesson10_task2.kt
@@ -1,6 +1,5 @@
 package org.example.lesson_10
 
-
 fun main() {
     println("Придумайте логин:")
     val login = readLine() ?: ""
@@ -8,13 +7,11 @@ fun main() {
     println("Придумайте пароль:")
     val password = readLine() ?: ""
 
-    if (validation(login) && validation(password)) {
+    if (isValid(login) && isValid(password)) {
         println("Добро пожаловать, $login!")
     } else {
         println("Логин или пароль недостаточно длинные. ")
     }
 }
 
-fun validation(input: String): Boolean {
-   return input.length >= 4
-}
+fun isValid (input: String) = input.length >= 4
