2d312b499d956c6752d0eaa30ff54ab9003fa77c
vkozhanova <kozhanova_vera@mail.ru>
2024-11-10 14:33:19 +0500

    KS-10-3

--- src/main/kotlin/lesson_10/lesson10_task3.kt
+++ src/main/kotlin/lesson_10/lesson10_task3.kt
@@ -1,28 +1,26 @@
 package org.example.lesson_10
 
-import kotlin.random.Random
-
 fun main() {
     println("Задайте количество символов для пароля.")
     val lengthPass = readln().toInt()
 
-    println(generate(lengthPass))
+    println(generatePassword(lengthPass))
 }
 
-fun generate(length: Int): String {
-    val numbers = ('0'..'9').toList()
-    val specialChars = "!#$%&'()*+,-./ ".toList()
+fun generatePassword(length: Int): String {
+    val numbers = '0'..'9'
+    val specialChars = "!#$%&'()*+,-./ "
 
-    val passBuilder = mutableListOf<Char>()
+    val passBuilder = StringBuilder()
 
     for (i in 0 until length) {
         if (i % 2 == 0) {
-            passBuilder.add(numbers[Random.nextInt(numbers.size)])
+            passBuilder.append(numbers.random())
         } else {
-            passBuilder.add(specialChars[Random.nextInt(specialChars.size)])
+            passBuilder.append(specialChars.random())
         }
     }
 
-    return passBuilder.joinToString("")
+    return passBuilder.toString()
 }
 
