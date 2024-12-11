cc49d31ec381acc38a468ddf54539efdb1807f4b
vkozhanova <kozhanova_vera@mail.ru>
2024-12-12 00:36:32 +0500

    KS-17-4

--- src/main/kotlin/lesson_17/lesson17_task4.kt
+++ src/main/kotlin/lesson_17/lesson17_task4.kt
@@ -1,28 +1,25 @@
 package org.example.lesson_17
 class Package(val trackingNumber: String) {
 
-    private var _currentLocation: String = "На складе"
+    private var _location: String = "На складе"
     private var _movementCount: Int = 0
 
-    val currentLocation: String
-        get() = _currentLocation
+    var location: String
+        get() = _location
+        set(value) {
+            _location = value
+            _movementCount++
+        }
 
     val movementCount: Int
         get() = _movementCount
-
-    var location: String
-        get() = _currentLocation
-        set(value) {
-            _currentLocation = value
-            _movementCount++
-        }
 }
 
 fun main() {
     val package1 = Package("12AB13")
-    println("Номер посылки: ${package1.trackingNumber}, текущее местоположение: ${package1.currentLocation}, счетчик перемещений: ${package1.movementCount}")
+    println("Номер посылки: ${package1.trackingNumber}, текущее местоположение: ${package1.location}, счетчик перемещений: ${package1.movementCount}")
 
     package1.location = "Пункт отбработки1"
     package1.location = "Пункт сортировки"
-    println("Номер посылки: ${package1.trackingNumber}, текущее местоположение: ${package1.currentLocation}, счетчик перемещений: ${package1.movementCount}")
+    println("Номер посылки: ${package1.trackingNumber}, текущее местоположение: ${package1.location}, счетчик перемещений: ${package1.movementCount}")
 }
