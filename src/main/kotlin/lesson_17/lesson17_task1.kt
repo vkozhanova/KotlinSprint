cfc967352bc8fd2ddf7be265e5f42acbf2853a0c
vkozhanova <kozhanova_vera@mail.ru>
2024-12-13 18:26:20 +0500

    KS-17-1

--- src/main/kotlin/lesson_17/lesson17_task1.kt
+++ src/main/kotlin/lesson_17/lesson17_task1.kt
@@ -3,6 +3,7 @@ package org.example.lesson_17
 class Quiz(_question: String, _answer: String) {
 
     val question: String = _question
+        get() = field
 
     var answer: String = _answer
         get() = field
