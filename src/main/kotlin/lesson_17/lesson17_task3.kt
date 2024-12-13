0f9977e991603ab2f75e5c023e1209023f88beac
vkozhanova <kozhanova_vera@mail.ru>
2024-12-13 17:08:10 +0500

    KS-17-3

--- src/main/kotlin/lesson_17/lesson17_task3.kt
+++ src/main/kotlin/lesson_17/lesson17_task3.kt
@@ -1,22 +1,26 @@
 package org.example.lesson_17
 
-class Folder(
-    private val name: String,
-    private var filesCount: Int,
-    private var isSecret: Boolean,
-) {
-    fun getFolderName(): String = if (isSecret) "Скрытая папка" else name
-    fun getFilesCount(): Int = if (isSecret) 0 else filesCount
+class Folder(name: String, _filesCount: Int) {
+
+    var isSecret: Boolean = false
+
+    val folderName: String = name
+        get() = if (isSecret) "Скрытая папка" else field
+
+    val filesCount: Int = _filesCount
+        get() = if (isSecret) 0 else field
 }
 
 fun main() {
 
-    val folder = Folder("Моя папка", 4, false)
-    val folder1 = Folder("ДискD", 12, true)
+    val folder = Folder("Моя папка", 4)
+    folder.isSecret = false
+    val folder1 = Folder("ДискD", 12)
+    folder1.isSecret = true
 
-    println("Имя папки: ${folder.getFolderName()}")
-    println("Количество файлов: ${folder.getFilesCount()}")
+    println("Имя папки: ${folder.folderName}")
+    println("Количество файлов: ${folder.filesCount}")
 
-    println("Имя папки: ${folder1.getFolderName()}")
-    println("Количество файлов: ${folder1.getFilesCount()}")
+    println("Имя папки: ${folder1.folderName}")
+    println("Количество файлов: ${folder1.filesCount}")
 }
