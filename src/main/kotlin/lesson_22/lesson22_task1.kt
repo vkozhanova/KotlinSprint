6f96e9dacad6bc4a1bebaaa56d4728fefae35e05
vkozhanova <kozhanova_vera@mail.ru>
2024-12-18 18:38:45 +0500

    KS-22-1

---
+++ src/main/kotlin/lesson_22/lesson22_task1.kt
@@ -0,0 +1,26 @@
+package org.example.lesson_22
+
+class RegularBook(
+    val name: String,
+    val author: String,
+)
+
+data class DataBook(
+    val name: String,
+    val author: String,
+)
+
+fun main() {
+    val regularBook1 = RegularBook("Воронья Дорога", "Иэн Бэнкс")
+    val regularBook2 = RegularBook("Воронья Дорога", "Иэн Бэнкс")
+
+    val dataBook1 = DataBook("Тревожные люди", "Фредерик Бакман")
+    val dataBook2 = DataBook("Тревожные люди", "Фредерик Бакман")
+
+    println(regularBook1 == regularBook2)
+//  false, потому, что обычные классы не переопределяют equals(),
+//  поэтому при сравнении экземпляров класса RegularBook сравниваются ссылки на объекты, а не свойства.
+    println(dataBook1 == dataBook2)
+//   true, потому, что data классы переопределяют метод equals(),
+//   и поэтому при сравнении экземпляров класса DataBook сравниваются свойства.
+}
