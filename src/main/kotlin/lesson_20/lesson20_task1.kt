73bbb753ac06e4617f4dfea8c8f0f5dc22291a00
vkozhanova <kozhanova_vera@mail.ru>
2024-12-23 16:28:24 +0500

    KS-20-1

--- src/main/kotlin/lesson_20/lesson20_task1.kt
+++ src/main/kotlin/lesson_20/lesson20_task1.kt
@@ -1,5 +1,5 @@
 package org.example.lesson_20
 
 fun main() {
-    println({it: String -> "С наступающим Новыи годом, $it!"}("userName"))
+    println({it: String -> "С наступающим Новым годом, $it!"}("userName"))
 }
