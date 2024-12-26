975ba05bed4b63b0ab512179ae51dd34e5e45dfc
vkozhanova <kozhanova_vera@mail.ru>
2024-12-26 14:13:55 +0500

    KS-22-4

--- src/main/kotlin/lesson_22/lesson22_task4.kt
+++ src/main/kotlin/lesson_22/lesson22_task4.kt
@@ -1,11 +1,11 @@
 package org.example.lesson_22
 
-data class MainScreenState(
-    val data: String = "",
-    val isLoading: Boolean = false,
-)
-
 class MainScreenViewModel {
+    data class MainScreenState(
+        val data: String = "",
+        val isLoading: Boolean = false,
+    )
+
     var mainScreenState = MainScreenState()
 
     fun loadData() {
